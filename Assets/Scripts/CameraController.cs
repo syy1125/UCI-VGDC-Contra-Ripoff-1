077a6fa5f87684b73269e64a6a1b0eb6dceae030
Eric Kim <ericpaulkim@gmail.com>
2017-10-18 18:56:07 -0700

    Just a bit of playing around with framework if we end up doing 2D

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+	public Vector3 offset;
+	public GameObject toFollow;
+
+	void LateUpdate () 
+	{
+		this.transform.position = toFollow.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	private Rigidbody2D rb;
+
+	public int jumpForce = 300;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update ()
+	{
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw ("Vertical");
+
+		rb.velocity = new Vector2(moveHorizontal, rb.velocity.y);
+
+		if (Input.GetButtonDown ("Jump"))
+			this.rb.AddForce ( new Vector2(0,jumpForce));
+
+	}
+}
