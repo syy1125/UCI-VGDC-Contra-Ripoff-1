672a90285fddbb8ed737cf0fc52d2b5bb3589299
syy1125 <syy1125@hotmail.com>
2017-10-23 22:45:00 -0700

    Implement score HUD
    Initial implementation of the score HUD.

---
+++ Assets/Scripts/ScoreHUD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreHUD : MonoBehaviour
+{
+	/// <summary>
+	/// A template string to be formatted with the score.
+	/// </summary>
+	private string _template;
+
+	private Text _textComponent;
+
+	private void Start()
+	{
+		_textComponent = gameObject.GetComponent<Text>();
+		
+		// The text component's current text is to be used as template.
+		_template = _textComponent.text;
+
+		SetScore(0);
+	}
+
+	public void SetScore(int score)
+	{
+		_textComponent.text = string.Format(_template, score);
+	}
+}
