7e047e112b2d86528c2f5b9d4095a9ace87181b5
syy1125 <syy1125@hotmail.com>
2017-10-23 22:47:01 -0700

    Add basic test for score
    Added basic test for scores to ensure that it works as intended.

---
+++ Assets/Scripts/ScoreTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class ScoreChangeEvent : UnityEvent<int>
+{
+}
+
+public class ScoreTest : MonoBehaviour
+{
+	public ScoreChangeEvent OnScoreChange;
+	
+	// Use this for initialization
+	void Start()
+	{
+		StartCoroutine(KeepAddingScore());
+	}
+
+	private IEnumerator KeepAddingScore()
+	{
+		for (int i = 0;; i++)
+		{
+			yield return new WaitForSeconds(0.1f);
+			OnScoreChange.Invoke(i);
+		}
+	}
+}
