3c659d7de5906f3ab88b9b43eebaf63e477bde83
syy1125 <syy1125@hotmail.com>
2017-10-23 21:32:22 -0700

    Add temporary health setting test
    Added a simple test to verify that setting health is working.

---
+++ Assets/Scripts/HealthTemp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class SetHealthEvent : UnityEvent<int>
+{
+}
+
+public class HealthTemp : MonoBehaviour
+{
+	public SetHealthEvent OnHealthChange;
+
+	private void Start()
+	{
+		Invoke("SetHealthLater", 5);
+	}
+
+	private void SetHealthLater()
+	{
+		OnHealthChange.Invoke(3);
+	}
+}
