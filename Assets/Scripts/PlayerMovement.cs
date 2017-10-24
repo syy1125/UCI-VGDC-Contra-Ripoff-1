3acd4e304276b1227b219e42257f51ad2731c7c0
Eric Kim <ericpaulkim@gmail.com>
2017-10-23 19:51:11 -0700

    Added Double Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour 
 {
 	private Rigidbody2D rb;
+	private BoxCollider2D col;
+
+	[SerializeField]private bool hasDoubleJump;
 
 	public int jumpForce = 150;
 	public int movementSpeed = 5;
 
 	void Start () 
 	{
+		hasDoubleJump = true;
 		rb = GetComponent<Rigidbody2D> ();
+		col = GetComponent<BoxCollider2D> ();
 	}
 
 	void Update ()
@@ -22,7 +27,34 @@ public class PlayerMovement : MonoBehaviour
 		rb.velocity = new Vector2(moveHorizontal * movementSpeed, rb.velocity.y);
 
 		if (Input.GetButtonDown ("Jump"))
-			this.rb.AddForce ( new Vector2(0,jumpForce));
+			AttemptJump ();
+	}
 
+	void AttemptJump()
+	{
+		if (isGrounded ())
+			hasDoubleJump = true;
+		else if (hasDoubleJump)
+			hasDoubleJump = false;
+		else
+			return;
+		Jump ();
+
+		
+	}
+
+	void Jump()
+	{
+		rb.velocity = new Vector2(rb.velocity.x, 0);
+		rb.AddForce ( new Vector2(0,jumpForce));
+	}
+
+	bool isGrounded()
+	{
+		Vector2 bottom = (Vector2)col.bounds.ClosestPoint (transform.position + Vector3.down * 3);
+		RaycastHit2D temp = Physics2D.Raycast(bottom, Vector2.down, 0.1f);
+		if(temp.transform != null && temp.transform.gameObject.CompareTag("Ground"))
+			return true;
+		return false;
 	}
 }
