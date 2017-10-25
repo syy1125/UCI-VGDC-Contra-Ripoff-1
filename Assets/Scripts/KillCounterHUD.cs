36dd8dd6bf130f0acb698d6bd442da6042a489a0
syy1125 <syy1125@hotmail.com>
2017-10-24 20:56:07 -0700

    Implement kill counter
    Implemented kill counter.

--- Assets/Scripts/KillCounterHUD.cs
+++ Assets/Scripts/KillCounterHUD.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreHUD : MonoBehaviour
+public class KillCounterHUD : MonoBehaviour
 {
+	private int _kills;
+
 	/// <summary>
 	/// Will automatically be set the the text component's content during initialization.
 	/// </summary>
 	private string _template;
+
 	private Text _textComponent;
 
 	private void Start()
@@ -16,11 +19,17 @@ public class ScoreHUD : MonoBehaviour
 		// The text component's current text is to be used as template.
 		_template = _textComponent.text;
 
-		SetScore(0);
+		SetKills(0);
 	}
 
-	public void SetScore(int score)
+	private void SetKills(int kills)
 	{
-		_textComponent.text = string.Format(_template, score);
+		_kills = kills;
+		_textComponent.text = string.Format(_template, _kills);
+	}
+
+	public void OnKill()
+	{
+		SetKills(_kills + 1);
 	}
 }
---
+++ Assets/Scripts/KillCounterTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KillCounterTest : MonoBehaviour
+{
+	public UnityEvent OnKill;
+
+	private void Start()
+	{
+		StartCoroutine(KeepAddingKills());
+	}
+
+	private IEnumerator KeepAddingKills()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(2);
+			OnKill.Invoke();
+		}
+	}
+}
