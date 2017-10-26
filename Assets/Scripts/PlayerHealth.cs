e426e3df1bdc80ae646635ae223204da6f8f0e60
David Jamgochian <jammer888@verizon.net>
2017-10-25 19:07:00 -0700

    Implement player health
    Created initial script for keeping track of player health.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int baseHealth = 3;   // made this public so it can be modified by the inspector tab.
+
+	private int health;
+
+	// Use this for initialization
+	void Start () {
+		health = baseHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Returns player's health
+	public int GetHealth()
+	{
+		return health;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		// if the object it collides with is tagged as an enemy, the player should losw 1 health point.
+		if (collision.collider.tag.Equals("Enemy"))
+			health--;
+
+		if (collision.collider.tag.Equals("HealthPickUp"))
+			health++;
+	}
+}
