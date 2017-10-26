73f3bf13011192c08d6a4be94ebb82a700c7921d
syy1125 <syy1125@hotmail.com>
2017-10-25 22:38:46 -0700

    Fix file name casing and add missing meta files
    (Hopefully) fixed git ignoring file name case.
    Added missing unity meta files.

---
+++ Assets/Scripts/ScoreHud.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreHud : MonoBehaviour
+{
+	/// <summary>
+	/// Will automatically be set the the text component's content during initialization.
+	/// </summary>
+	private string _template;
+	private Text _textComponent;
+
+	private void Start()
+	{
+		_textComponent = gameObject.GetComponent<Text>();
+		
+		// The text component's current text is to be used as template.
+		_template = _textComponent.text;
+
+		SetScore(0);
+	}
+
+	public void SetScore(int score)
+	{
+		_textComponent.text = string.Format(_template, score);
+	}
+}
