fca2dff4becc0538335008eabb6527a8970e56a4
Anthony Ha <haa4@uci.edu>
2017-10-29 23:23:27 -0700

    Added stationary enemy, enemy that moves to player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Class with common traits of all enemies
+//(Most) Enemies have health, and die when it reaches 0
+
+public class Enemy : MonoBehaviour {
+    public int baseHealth = 1;
+    private int Health;
+	// Use this for initialization
+	void Start () {
+        Health = baseHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Health == 0)
+            die();
+	}
+
+    void die()
+    {
+        Destroy(this.gameObject);
+    }
+}
