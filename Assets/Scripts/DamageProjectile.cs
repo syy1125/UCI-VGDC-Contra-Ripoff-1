13a094fbb345c8725f0deeddc7aef815d67de31b
Jack Sun <syy1125@hotmail.com>
2017-11-05 14:34:41 -0800

    Add projectile lifespan
    Now, by default, projectiles will expire and disappear after 30 seconds.

--- Assets/Scripts/DamageProjectile.cs
+++ Assets/Scripts/DamageProjectile.cs
@@ -1,11 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Remoting.Lifetime;
 using UnityEngine;
 
 public class DamageProjectile : MonoBehaviour
 {
 	public bool Hostile;
 	public int Damage = 1;
+
+	/// <summary>
+	/// Amount of time, in seconds, before the projectile disappears.
+	/// Set to negative value to make projectile never expire.
+	/// </summary>
+	public float Lifespan = 30;
+
+	private float _startTime;
+
+	private void Start()
+	{
+		_startTime = Time.time;
+	}
+
+	private void Update()
+	{
+		if (Lifespan > 0 && Time.time > _startTime + Lifespan)
+		{
+			Destroy(gameObject);
+		}
+	}
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
