6abbb4fda8ec9432606a02490eb13318f988614e
Anthony Ha <haa4@uci.edu>
2017-11-20 16:10:38 -0800

    Added Invincibility time to PlayerHealth after colliding with Enemy

--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -1,7 +1,10 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 public class PlayerHealth : AbstractHealth
 {
+    bool Invincible = false;
+    int InvincibilityTime = 3; //how long player is invincible in seconds
 	private void Reset()
 	{
 		MaxHealth = 3;
@@ -12,4 +15,18 @@ public class PlayerHealth : AbstractHealth
 		// Placeholder text until formal player death mechanism are discussed.
 		Debug.Log("Player died!");
 	}
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Collided w/ Enemy");
+            Damage(collision.gameObject, 1);
+            Invincible = true;
+            Invoke("ResetInvincibility", InvincibilityTime);
+        }
+    }
+    void ResetInvincibility()
+    {
+        Invincible = false;
+    }
 }
