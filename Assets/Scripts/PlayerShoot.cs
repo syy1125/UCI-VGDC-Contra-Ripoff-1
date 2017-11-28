482d432babc41957469493d59d9f88bca29c13aa
Avineesh Kompella <AvineeshKom@gmail.com>
2017-11-27 19:11:29 -0800

    ammo counter
    added ammo counter and also increased starting amo to 6. though it's public anyway

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -6,18 +6,26 @@ using UnityEngine.UI;
 public class PlayerShoot : MonoBehaviour {
 
     public Transform firePoint;
-    public int ammo = 3;
+    public int ammo;
     public GameObject Projectile;
+	public Text ammocount;
 
 
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 
     // Update is called once per frame
     void Update()
     {
+		if (ammo > 0) {
+			ammocount.color = Color.white;
+			ammocount.text = ammo.ToString ();
+		} else {
+			ammocount.color = Color.red;
+			ammocount.text = "Right click to reload!";
+		}
         if (Input.GetMouseButtonDown(0))
         {
             shootBullet();
@@ -26,7 +34,7 @@ public class PlayerShoot : MonoBehaviour {
         if (Input.GetMouseButtonDown(1))
         {
             Debug.Log("Reloading...");
-            ammo = 3;
+			ammo = 6;
             Debug.Log("Reloaded.");
         }
     }
