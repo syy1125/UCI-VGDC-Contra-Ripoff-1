7e569823d05a51cf440e1e60b4ed1a3f64d2d481
David Jamgochian <jammer888@verizon.net>
2017-11-28 21:40:38 -0800

    Small updates to boss prefab and EnemyMovement
    contains small updates boss prefab values, and slightly modified
    EnemyMovement concerning jumping and jumping cooldown.

--- Assets/Scripts/Movement/EnemyMovement.cs
+++ Assets/Scripts/Movement/EnemyMovement.cs
@@ -119,9 +119,11 @@ public class EnemyMovement : AbstractMovement
 
 		if (ShouldJump() && jumpCooldown <= 0)
 		{
-			AttemptJump();
-			jumpCooldown = timeBetweenJumps;
-			StartCoroutine("JumpCD");
+			if (AttemptJump())
+			{
+				jumpCooldown = timeBetweenJumps;
+				StartCoroutine("JumpCD");
+			}
 		}
 
 //		if (ShouldFall())
@@ -240,14 +242,15 @@ public class EnemyMovement : AbstractMovement
 		return jump;
 	}
 
-	void AttemptJump()
+	bool AttemptJump()
 	{
 		if (!IsGrounded())
 		{
 			Debug.Log(name + " was not grounded.");
-			return;
+			return false;
 		}
 		Jump();
+		return true;
 	}
 
 	void Jump()
