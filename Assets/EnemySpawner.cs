78ce64bb1cca492d390f0719aa76e00d2a89ce27
EPKgit <ericpaulkim@gmail.com>
2017-12-04 18:10:59 -0800

    Added Enemy spawners

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour 
+{
+
+	public GameObject EnemyToCreate;
+
+	private float CreationRadius;
+	private Camera main;
+	private CircleCollider2D col;
+	private bool spawned;
+
+	void Start () 
+	{
+		col = GetComponent<CircleCollider2D>();
+		/*main = Camera.main;
+		Vector3 edgeOfScreen = main.ScreenToWorldPoint(new Vector2(main.pixelWidth/2,0));
+		//CreationRadius = main.ScreenToWorldPoint();
+		Debug.Log(edgeOfScreen);
+		//col.radius = CreationRadius;
+		*/
+		spawned = false;
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(!spawned && other.gameObject.tag == "Player")
+		{
+			spawned = true;
+			Instantiate(EnemyToCreate, this.transform.position, Quaternion.identity);
+		}
+	}
+}
